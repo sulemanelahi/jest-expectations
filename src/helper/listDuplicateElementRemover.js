a46e3173b26e4e565eb44179fe92eedf6e4979f7
Suleman Elahi <sulemane010@gmail.com>
2023-06-19 17:10:39 +0500

    🐛 fix: Fix a bug in duplicate Remover

--- src/helper/listDuplicateElementRemover.js
+++ src/helper/listDuplicateElementRemover.js
@@ -1,3 +1,5 @@
-module.exports.duplicateRemover = characters.filter((element, index) => {
+module.exports.duplicateRemover = (characters) => {
+  characters.filter((element, index) => {
     return characters.indexOf(element) === index;
-});
+  });
+};
